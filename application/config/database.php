e3404032e362be5e97b3510fcdd1af37ff537bdc
niktechsoftware <info@niktechsoftware.com>
2019-03-12 02:52:12 +0000

    12/3/19 8:21 A.M

--- application/config/database.php
+++ application/config/database.php
@@ -50,13 +50,13 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'cashon_wallet';
+// $db['default']['username'] = 'root';
+// $db['default']['password'] = '';
+// $db['default']['database'] = 'cashon_wallet';
 
-//  $db['default']['username'] = 'cashowdd_wallet';
-// $db['default']['password'] = 'Rahul!123singh';
-// $db['default']['database'] = 'cashowdd_wallet';
+$db['default']['username'] = 'cashowdd_wallet';
+$db['default']['password'] = 'Rahul!123singh';
+$db['default']['database'] = 'cashowdd_wallet';
 
 
 
